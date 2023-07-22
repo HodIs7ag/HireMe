4b9696cd9a7a3a2c911fee4ff07bc46ba9b44ac9
Hodis7ag <mahmoud.abdelrahim@studenti.unime.it>
2023-07-22 19:57:29 +0200

    first model

--- job/models.py
+++ job/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+'''
+django model fileds:
+    - html widget
+    - validation
+    - db size
+'''
+
 # Create your models here.
+class Job(models.Model): #table
+    title = models.CharField(max_length=100)    #column
