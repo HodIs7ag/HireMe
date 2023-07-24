6cf77cbb7095cb27e120ad6406e89d13961388fd
Hodis7ag <mahmoud.abdelrahim@studenti.unime.it>
2023-07-24 13:07:32 +0200

    update models

--- job/models.py
+++ job/models.py
@@ -7,6 +7,21 @@ django model fileds:
     - db size
 '''
 
+JOB_TYPE = (
+    ('Full Time', 'Full Time'),
+    ('Part Time', 'Part Time'),
+)
+
 # Create your models here.
 class Job(models.Model): #table
     title = models.CharField(max_length=100)    #column
+    #location
+    job_type = models.CharField(max_length=15, choices= JOB_TYPE)
+    description = models.TextField(max_length=1000)
+    published_at = models.DateTimeField(auto_now=True)
+    vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    experience = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
