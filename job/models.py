ebede86f11407890b5546fad85639aa8e2b5e603
Hodis7ag <mahmoud.abdelrahim@studenti.unime.it>
2023-07-30 14:08:55 +0200

    pagination

--- job/models.py
+++ job/models.py
@@ -12,6 +12,10 @@ JOB_TYPE = (
     ('Part Time', 'Part Time'),
 )
 
+def image_upload(instance, filename):
+    imagename, extension = filename.split(".")
+    return 'jobs/%s.%s'%(instance.id,extension)
+
 # Create your models here.
 class Job(models.Model): #table
     title = models.CharField(max_length=100)    #column
@@ -22,6 +26,21 @@ class Job(models.Model): #table
     vacancy = models.IntegerField(default=1)
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=0)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=image_upload)
 
     def __str__(self):
         return self.title
+    
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+'''
+Relations:
+    - One to Many   [author - posts]
+    - Many to Many  [user - groups]
+    - One to one    [user - profile]
+'''
--- job/views.py
+++ job/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
+from .models import Job
 
 # Create your views here.
+
+def job_list(request):
+    job_list = Job.objects.all()
+    paginator = Paginator(job_list, 1)  # Show 10 jobs per page.
+    page_number = request.GET.get("page")
+    jobs_page_obj = paginator.get_page(page_number)
+    context = {
+        'jobs':jobs_page_obj
+    }
+    return render(request, 'job/job_list.html', context)
+
+def job_detail(request, id):
+    job_detail = Job.objects.get(id = id)
+    context = {
+        'job' : job_detail
+    }
+    return render(request, 'job/job_detail.html', context)
+
